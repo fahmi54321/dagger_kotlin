3db652571efa19784f05e42648dc4fa473eeda74
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2025-09-16 09:55:11 +0700

    base class for mvc views

--- app/src/main/java/com/techyourchance/dagger2course/screens/common/viewsmvc/BaseViewMvc.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/common/viewsmvc/BaseViewMvc.kt
@@ -11,13 +11,12 @@ import java.util.HashSet
 open class BaseViewMvc<LISTENER_TYPE>(
     private val layoutInflater: LayoutInflater,
     private val viewGroup: ViewGroup?,
-    @LayoutRes private val  layoudId: Int) {
+    @LayoutRes private val layoudId: Int) {
 
     protected val listeners = HashSet<LISTENER_TYPE>()
     protected val context: Context get() = rootView.context
 
     val rootView:View = layoutInflater.inflate(layoudId,viewGroup,false)
-
 
     fun registerListener(Listener: LISTENER_TYPE){
         listeners.add(Listener)
