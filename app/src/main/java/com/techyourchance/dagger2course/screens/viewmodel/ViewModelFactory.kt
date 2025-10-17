7b93aa5eae02ed3c642be1450cd38f307f4ac889
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2025-10-17 20:56:07 +0700

    Multibinding

--- app/src/main/java/com/techyourchance/dagger2course/screens/viewmodel/ViewModelFactory.kt
+++ app/src/main/java/com/techyourchance/dagger2course/screens/viewmodel/ViewModelFactory.kt
@@ -6,14 +6,14 @@ import javax.inject.Inject
 import javax.inject.Provider
 
 class ViewModelFactory @Inject constructor (
-    val myViewModelProvider: Provider<MyViewModel>,
-    val myViewModel2Provider: Provider<MyViewModel2>
+    val providers: Map<Class<out ViewModel>, @JvmSuppressWildcards Provider<ViewModel>>
 ): ViewModelProvider.Factory{
     override fun <T : ViewModel> create(modelClass: Class<T>): T {
-        return when {
-            modelClass.isAssignableFrom(MyViewModel::class.java) -> myViewModelProvider.get() as T
-            modelClass.isAssignableFrom(MyViewModel2::class.java) -> myViewModel2Provider.get() as T
-            else -> throw IllegalArgumentException("Unsupported ViewModel type: $modelClass")
+        val provider = providers[modelClass]
+        if (provider != null){
+            return provider.get() as T
+        }else{
+            throw RuntimeException("unsupported viewmodel type: $modelClass")
         }
     }
 }
