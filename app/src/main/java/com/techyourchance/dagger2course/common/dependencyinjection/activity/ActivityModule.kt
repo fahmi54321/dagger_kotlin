b29970823454846e13c946dc1e106613da74194b
fahmiabdulaziz44 <fahmiabdulaziz44@gmail.com>
2025-10-23 09:14:31 +0700

    Providing AppCompatActivity

--- app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/activity/ActivityModule.kt
+++ app/src/main/java/com/techyourchance/dagger2course/common/dependencyinjection/activity/ActivityModule.kt
@@ -1,5 +1,6 @@
 package com.example.daggertwo.common.dependencyinjection.activity
 
+import android.app.Activity
 import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import com.example.daggertwo.screens.common.ScreensNavigator
@@ -19,6 +20,9 @@ abstract class ActivityModule {
     abstract fun screensNavigator(screensNavigatorImpl: ScreensNavigatorImpl): ScreensNavigator
 
     companion object{
+        @Provides
+        fun appCompatActivity(activity: Activity): AppCompatActivity = activity as AppCompatActivity
+
         @Provides
         fun fragmentManager(activity: AppCompatActivity) = activity.supportFragmentManager
 
